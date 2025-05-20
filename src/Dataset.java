82daf0641780b04ff428f2d008342310385689a3
aasergiopinto <sergio.pinto1@mail.udp.cl>
2025-05-20 13:46:05 -0400

    Create Dataset.java

---
+++ src/Dataset.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class Dataset{
+    
+	ArrayList<Game> data;
+	String sortedByAttribute;
+	
+	Dataset(ArrayList<Game> data) this.data = data;
+	    
+	public static int getGamesByPrice(int price){
+	    
+	    
+	}
+	
+	
+	
+	
+}
