ccc1930f78184c4af16ebfc29d3035ae0de43820
JoakinMacAuliffe <jfmacauliffe@gmail.com>
2025-05-20 13:27:40 -0400

    hola

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,37 @@
 public class Game {
 
+    private String name;
+    private String category;
+    private int price = 0;
+    private int quality = 0;
 
+    Game(String name, String category, int price, int quality) {
+        this.name = name;
+        this.category = category;
 
+        if (price >= 0) {
+           this.price = price;
+        }
+
+        if (quality >= 0 && quality <= 100) {
+            this.quality = quality;
+        }
+
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getQuality() {
+        return quality;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
