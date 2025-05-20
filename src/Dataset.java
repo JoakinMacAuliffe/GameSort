36f24cc14c2b294afd0fdd0a00a6637796a1fad0
aasergiopinto <sergio.pinto1@mail.udp.cl>
2025-05-20 14:12:53 -0400

    Update Dataset.java

--- src/Dataset.java
+++ src/Dataset.java
@@ -1,16 +1,33 @@
-import java.util.Arrays;
-import java.util.ArrayList;
+import java.util.*;
 
 public class Dataset{
 
 	private ArrayList<Game> data;
 	private String sortedByAttribute;
 	
-	Dataset(ArrayList<Game> data){
-		this.data = data;
-	}
+	Dataset(ArrayList<Game> data) this.data = data;
 	    
-	public static int getGamesByPrice(int price){
+	public ArrayList<Game> getGamesByPrice(int price){
+	    
+	    ArrayList<Game> lista = new ArrayList<>();
+	    
+	    if (this.sortedByAttribute == "price") {
+	        
+	        int left = 0;
+	        int right = data.size()-1;
+	        
+	        while (left <= right) {
+	            
+	            int mid = (left + right) / 2;
+	            
+	            if (mid == "price") return mid;
+	            
+	            else if (mid > left) {
+	                   
+	                   
+	               }
+	        }
+	    }
 
 	}
 
