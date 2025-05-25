da365e5bb7daa0a8b1401caa1977ac678c32d1d4
JoakinMacAuliffe <jfmacauliffe@gmail.com>
2025-05-25 17:18:07 -0400

    ready

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,27 @@
+import java.awt.font.GlyphMetrics;
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
 
         GenerateData generateData = new GenerateData();
-        ArrayList<Game> data1 = generateData.generateData(100);
+        ArrayList<Game> data1 = generateData.generateData(1000);
 
         Dataset dataset = new Dataset(data1);
 
         ArrayList<Game> data = dataset.getData();
 
-        dataset.sortByAlgorithm("insertionsort", "price");
+        dataset.sortByAlgorithm("bubblesort", "price");
 
-        for(int i = 0; i < data.size(); i++) {
-            System.out.println(i+": " + "$" + data.get(i).getPrice() + ", " + data.get(i).getCategory() + ", " + data.get(i).getQuality());
-        }
-
-        ArrayList<Game> data2 = dataset.getGamesByQuality(30);
-
-//        for(int i = 0; i < data2.size(); i++) {
-//            System.out.println(data2.get(i).getQuality());
+//        for(int i = 0; i < data.size(); i++) {
+//            System.out.println(i+": " + "$" + data.get(i).getPrice() + ", " + data.get(i).getCategory() + ", " + data.get(i).getQuality());
 //        }
+
+        ArrayList<Game> data2 = dataset.getGamesByPriceRange(3000, 10000);
+
+        for(int i = 0; i < data2.size(); i++) {
+            System.out.println(i+": " + "$" + data2.get(i).getPrice() + ", " + data2.get(i).getCategory() + ", " + data2.get(i).getQuality());
+        }
 
     }
 
